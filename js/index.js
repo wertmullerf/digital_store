8bac310f93c3c582d43c29a17cfa14dfe2c2eb77
Luca Vitorino <valentinvitorimo28@gmail.com>
2022-12-29 19:29:46 -0300

    Navbar finished

--- js/index.js
+++ js/index.js
@@ -3,3 +3,11 @@ window.addEventListener("scroll", function () {
     header.classList.toggle("sticky", this.window.scrollY > 0)
 
 })
+const navbar = document.querySelector(".navbar")
+const openNav = document.querySelector("#burger-menu").addEventListener("click", () => {
+    navbar.classList.toggle("inactive")
+})
+const closeNav = document.querySelector("#closeNav").addEventListener("click", () => {
+    navbar.classList.toggle("inactive")
+    body.classList.toggle("opacity")
+})
